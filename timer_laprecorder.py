dc4d16ecc6cfeb62cfa578e4b7d975b4354fefe1
hadacchi <hadacchi@users.noreply.github.com>
2016-06-10 12:52:10 +0900

    initial commit

---
+++ timer_laprecorder.py
@@ -0,0 +1,26 @@
+import time
+import sys
+
+t0   = time.time()
+s    = t0
+page = 0
+
+with open('log'+str(t0)+'.log','w') as fobj:
+    while(True):
+        c = sys.stdin.readline()
+        if c[0] == 'q':
+            break
+        elif c[0].isnumeric():
+            page = int(c)
+        else:
+            page += 1
+        t1 = time.time()
+        fobj.write('{0} {1}\n'.format(page,t1-t0))
+        print(page,t1-t0)
+        t0 = t1
+    page += 1
+    t1 = time.time()
+    fobj.write('{0} {1}\n'.format(page,t1-t0))
+    print(page,t1-t0)
+    fobj.write('total {0}\n'.format(t1-s))
+    print('total',t1-s)
